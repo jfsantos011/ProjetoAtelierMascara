51fcb4b87fcedec4af7f468a5bbad56fe90f844e
jfsantos011 <72039878+jfsantos011@users.noreply.github.com>
2020-09-29 10:43:23 -0300

    CRUD da aplicação
    Adicionar o recurso (scaffolding) que gera o CRUD da aplicação.

---
+++ Projeto.AtelierVera.WEB/Atelier.Mascara.Dados.Entity/Migrations/202009290339069_MigracaoInical.cs
@@ -0,0 +1,29 @@
+﻿namespace Atelier.Mascara.Dados.Entity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigracaoInical : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TipoMascara",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 100),
+                        Tecido = c.String(nullable: false, maxLength: 50),
+                        Cor = c.String(nullable: false, maxLength: 50),
+                        Descricao = c.String(nullable: false, maxLength: 1000),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TipoMascara");
+        }
+    }
+}
---
+++ Projeto.AtelierVera.WEB/Projeto.AtelierVera.WEB/Controllers/TipoMascarasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Atelier.Mascara.Dados.Entity.Context;
+using Atelier.Mascara.Dominio;
+
+namespace Projeto.AtelierVera.WEB.Controllers
+{
+    public class TipoMascarasController : Controller
+    {
+        private MascaraDBContext db = new MascaraDBContext();
+
+        // GET: TipoMascaras
+        public ActionResult Index()
+        {
+            return View(db.TiposDeMascaras.ToList());
+        }
+
+        // GET: TipoMascaras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TipoMascara tipoMascara = db.TiposDeMascaras.Find(id);
+            if (tipoMascara == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipoMascara);
+        }
+
+        // GET: TipoMascaras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoMascaras/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Tecido,Cor,Descricao")] TipoMascara tipoMascara)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TiposDeMascaras.Add(tipoMascara);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tipoMascara);
+        }
+
+        // GET: TipoMascaras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TipoMascara tipoMascara = db.TiposDeMascaras.Find(id);
+            if (tipoMascara == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipoMascara);
+        }
+
+        // POST: TipoMascaras/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Tecido,Cor,Descricao")] TipoMascara tipoMascara)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tipoMascara).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tipoMascara);
+        }
+
+        // GET: TipoMascaras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TipoMascara tipoMascara = db.TiposDeMascaras.Find(id);
+            if (tipoMascara == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tipoMascara);
+        }
+
+        // POST: TipoMascaras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TipoMascara tipoMascara = db.TiposDeMascaras.Find(id);
+            db.TiposDeMascaras.Remove(tipoMascara);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
